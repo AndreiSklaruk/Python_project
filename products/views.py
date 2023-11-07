ecad954c0b32365d2241f542da9f9c52459d9055
AndreiSklaruk <andrei.sklaruk@gmail.com>
2023-11-07 14:36:13 +0300

    first commit

---
+++ products/views.py
@@ -0,0 +1,20 @@
+from django.http import request
+from django.shortcuts import render
+from products.models import *
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'description': "Description of the home page",
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
