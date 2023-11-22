cb6b718ac938e703c7526bcc9d30bd6add5b7720
AndreiSklaruk <andrei.sklaruk@gmail.com>
2023-11-22 15:44:23 +0300

    Finish basik lavel - working project

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser')
+    list_display_links = ('username', 'first_name', 'last_name', 'email')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username', 'first_name', 'last_name', 'email')
